01db8cde1e55dbfbc0678dc1905d2faed03235ef
polina <polinakozlova324@gmail.com>
2023-05-16 18:40:32 +0300

    taskAdditional_modified

--- src/homework/taskAdditional/Apple.java
+++ src/homework/taskAdditional/Apple.java
@@ -2,4 +2,7 @@ package homework.taskAdditional;
 
 public class Apple {
     private String color = "Green";
+    void show(){
+        System.out.println("Color: " + color);
+    }
 }
--- src/homework/taskAdditional/Main.java
+++ src/homework/taskAdditional/Main.java
@@ -5,13 +5,14 @@ import java.lang.reflect.Field;
 public class Main {
     public static void main(String[] args) {
         Apple apple = new Apple();
+        apple.show();
         try {
             Field field = apple.getClass().getDeclaredField("color");
             field.setAccessible(true);
             field.set(apple, "Red");
-            System.out.println(field.get(apple));
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
+        apple.show();
     }
 }
